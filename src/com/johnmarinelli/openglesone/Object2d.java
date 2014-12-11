4c3fb5f7eea6cad9a5b8030126bca1aa2ca53221
johnmarinelli <jonmarinelli@hotmail.com>
2014-12-10 21:38:02 -0700

    added motions

--- src/com/johnmarinelli/openglesone/Object2d.java
+++ src/com/johnmarinelli/openglesone/Object2d.java
@@ -19,8 +19,21 @@ public abstract class Object2d {
 	/* number of coordinates per vertex */
 	static final int COORDS_PER_VERTEX = 3;
 	
+	/* orientation matrices */
+	private Vector3d mPosition = new Vector3d(0, 0, 0);
+	private Vector3d mRotationAxis = new Vector3d(0, 0, 0);
+	private Vector3d mScale = new Vector3d(1, 1, 1);
+	
+	private float[] mPositionMatrix = new float[16];
+	private float[] mRotationMatrix = new float[16];
+	private float mRotationAngle = 0f;
+	private float[] mScaleMatrix = new float[16];
+	
 	/* model matrix */
 	private float[] mModelMatrix = new float[16];
+	
+	/* model view matrix */
+	private float[] mModelViewMatrix = new float[16];
 	
 	/* model-view-projection matrix */
 	private float[] mMVPMatrix = new float[16];
@@ -90,8 +103,50 @@ public abstract class Object2d {
 	}
 	
 	public void draw(Camera camera) {
+		/* set position vector */
+		mPosition.mX = 0f;
+		mPosition.mY = 0f;
+		mPosition.mZ = 0f;
+		
+		/* set rotation vector */
+		mRotationAxis.mX = 0f;
+		mRotationAxis.mY = 0f;
+		mRotationAxis.mZ = 1f;
+		
+		/* set scale vector */
+		mScale.mX = 1f;
+		mScale.mY = 1f;
+		mScale.mZ = 1f;
+		
 		float[] projectionMatrix = camera.getProjectionMatrix();
 		float[] viewMatrix = camera.getViewMatrix();
+		
+		/* build translation matrix */
+		Matrix.setIdentityM(mPositionMatrix, 0);
+		Matrix.translateM(mPositionMatrix, 0, mPosition.mX, mPosition.mY, mPosition.mZ);
+		
+		/* build rotation matrix */
+		Matrix.setIdentityM(mRotationMatrix, 0);
+		Matrix.rotateM(mRotationMatrix, 0, 
+				   mRotationAngle++, 
+				   mRotationAxis.mX, 
+				   mRotationAxis.mY, 
+				   mRotationAxis.mZ);
+		
+		/* build scale matrix */
+		Matrix.setIdentityM(mScaleMatrix, 0);
+		Matrix.scaleM(mScaleMatrix, 0, mScale.mX, mScale.mY, mScale.mZ);
+		
+		float[] tempMatrix = new float[16];
+   
+		/* rotate object around axis, then translate */
+		Matrix.multiplyMM(tempMatrix, 0, mPositionMatrix, 0, mRotationMatrix, 0);
+		
+		/* build scale matrix */
+		Matrix.setIdentityM(mModelMatrix, 0);
+
+		/* scale then store in model matrix */
+		Matrix.multiplyMM(mModelMatrix, 0, tempMatrix, 0, mScaleMatrix, 0);
 		
 		/* add program to gles environment */
 		GLES20.glUseProgram(mProgram);
@@ -118,10 +173,15 @@ public abstract class Object2d {
 		
 		/* get handle to shape's transformation matrix */
 		mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
-		/*
-		 * calculate projection & view transformation
-		 */
-		Matrix.multiplyMM(mMVPMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+
+		/* build model view matrix */
+	    Matrix.multiplyMM(mModelViewMatrix, 0, viewMatrix, 0, mModelMatrix, 0);
+	     
+	    /* build MVP matrix */
+	    Matrix.multiplyMM(mMVPMatrix, 0, projectionMatrix, 0, mModelViewMatrix, 0);
+	    
+		/* calculate projection & view transformation */
+		//Matrix.multiplyMM(mMVPMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
 		
 		/* pass projection & view transformation into shader */
 		GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mMVPMatrix, 0);
